20af81488759f789a89427fcc72257737d772e2e
gehaifu <16964544@qq.com>
2025-11-21 09:23:53 +0800

    create2factory

---
+++ contracts/Create2Factory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract Child {
+    string public message;
+    constructor(string memory _message){
+        message = _message;
+    }
+    function setMessage(string calldata _message) public{
+        message = _message;
+    }
+}
+
+contract Create2Factory {
+    function deployContract(bytes32 _salt, string calldata _message) public returns(address) {
+        bytes memory bytecode = type(Child).creationCode;
+        bytes memory constructorArgs = abi.encodePacked(_message);
+        bytes memory fullyBytecode = abi.encodePacked(bytecode, constructorArgs);
+        address deployedAddress;
+        assembly {
+            deployedAddress := create2(0, add(fullyBytecode, 0x20), mload(fullyBytecode), _salt)
+        }
+        require(deployedAddress != address(0), "Deployment failed");
+        return deployedAddress;
+    }
+    function computeAddress(bytes32 _salt, string calldata _message) public view returns(address){
+        bytes memory bytecode = type(Child).creationCode;
+        bytes memory constructionArgs = abi.encodePacked(_message);
+        bytes memory fullBytecode = abi.encodePacked(bytecode, constructionArgs);
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(fullBytecode)));
+        // hash：64位16进制数， address：40位16进制数，先转uint256,然后用uint160低位截断，得到后20位byte，再转address
+        return address(uint160(uint256(hash)));
+    }
+}
