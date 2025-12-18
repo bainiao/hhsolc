d8a428ad4753105c4b2404697c0854e4dff575c2
gehaifu <16964544@qq.com>
2025-12-18 09:30:23 +0800

    update job websites

--- contracts/abiCall.sol
+++ contracts/abiCall.sol
@@ -31,30 +31,26 @@ contract DataConsumer {
         return abi.decode(data, (string));
     }
 
-    function setDataByABI1(string calldata newData) public returns (bool) {
+    function setDataByABI1(string calldata newData) public returns (bool success) {
         // playload
         bytes memory payload = abi.encodeWithSignature("setData(string)", newData);
-        (bool success, ) = dataStorageAddress.call(payload);
-
-        return success;
+        (success, ) = dataStorageAddress.call(payload);
     }
 
-    function setDataByABI2(string calldata newData) public returns (bool) {
+    function setDataByABI2(string calldata newData) public returns (bool success) {
         console.log("dataStorageAddress:", dataStorageAddress); // 打印地址
         require(dataStorageAddress != address(0), "dataStorageAddress is zero"); // 增加检查
         // selector
         bytes4 selector = bytes4(abi.encodePacked(keccak256("setData(string)")));
         // playload
         bytes memory payload = abi.encodeWithSelector(selector, newData);
-        (bool success, ) = dataStorageAddress.call{gas:100000}(payload);
+        (success, ) = dataStorageAddress.call{gas:100000}(payload);
         require(success, "setDataByABI2: call failed"); 
-        return success;
     }
 
-    function setDataByABI3(string calldata newData) public returns (bool) {
+    function setDataByABI3(string calldata newData) public returns (bool success) {
         // playload
         bytes memory payload = abi.encodeCall(DataStorage(dataStorageAddress).setData, (newData));
-        (bool success, ) = dataStorageAddress.call(payload);
-        return success;
+        (success, ) = dataStorageAddress.call(payload);
     }
 }
--- contracts/abiCall.t.sol
+++ contracts/abiCall.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import {DataConsumer, DataStorage} from "./test.sol";
+import {DataConsumer, DataStorage} from "./abiCall.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract DataConsumerTest is Test {
